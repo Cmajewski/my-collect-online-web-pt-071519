59201d20469700e580dc966b6b7564ca60bf4540
Caitlin Majewski <cmajewski@wesleyan.edu>
2019-08-02 19:42:57 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,4 +1,5 @@
 collection=["Tim Jones", "Tom Smith", "Jim Campagno"]
+yield(['ruby', 'javascript', 'python', 'objective-c']) {|language| lang.upcase}
 
 def my_collect(collection)
   i=0
@@ -8,5 +9,3 @@ while i<correction.length
   i=i+1
 end
 end
-
-yield(['ruby', 'javascript', 'python', 'objective-c']) {|language| lang.upcase}
