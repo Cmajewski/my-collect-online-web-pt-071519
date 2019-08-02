bde3c8b45c1eea81688f6c3d06e0cd2e64e62f70
Caitlin Majewski <cmajewski@wesleyan.edu>
2019-08-02 19:34:26 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,9 +1,12 @@
-def my_collect(array)
+collection=["Tim Jones", "Tom Smith", "Jim Campagno"]
+collection = ['ruby', 'javascript', 'python', 'objective-c']
+def my_collect(collection)
   i=0
   new_collection=[]
-while i<array.length
+while i<correction.length
   new_collection<<yield
   i=i+1
 end
-array
 end
+
+yield(collection) {|language| lang.upcase}
